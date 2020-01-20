465b1d453ba2040533434f20ee827076ab732286
Diogo Righi <diogorighi@gmail.com>
2020-01-20 17:13:57 -0300

    basic routing configuration for menu-items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
 import Homepage from "./pages/homepage/homepage.component";
 
+function HatsPage() {
+  return <h1>Hats Page</h1>;
+}
+
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Switch>
+      <Route exact path="/" component={Homepage} />
+      <Route path="/shop/hats" component={HatsPage} />
+    </Switch>
   );
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -42,8 +42,8 @@ function Directory() {
   const [sections] = useState(sectionsMock);
   return (
     <div className="directory-menu">
-      {sections.map(({ title, imageUrl, id, size }) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
       ))}
     </div>
   );
